1bb48134056128d7298fd6b09ea0ae4f4f02b8af
Supaluck Singjan <supalucksingjan@gmail.com>
2020-07-08 05:11:05 +0700

    Add Customers Structure

--- mux-http.go
+++ mux-http.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+type Customers struct {
+	
+	FFullName string `json:"fullName"`
+
+}
+
+var data []Customers = []Customers{}
 
 func main() {
-	router := mux.NewRouter()	
-	router.HandleFunc("/customers", addItem)
+	router := mux.NewRouter() // r = router
+	router.HandleFunc("/customers", addItem).Methods("POST")
 	http.ListenAndServe(":8888", router)
 }
 
-func addItem(w http.ResponseWriter, r*http.Request) {
-	w.Write([]byte("TEST"))
+func addItem(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("TEST"))
+	var newData Customers
+	json.NewDecoder(r.Body).Decode(&newData)
+	data = append(data, newData)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+	
 }
